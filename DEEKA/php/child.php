8a0d30e9ae68b6c825ee88769743ae6ba5a0bbb1
thisrishabh <106669692+thisrishabh@users.noreply.github.com>
2022-09-14 13:15:51 +0530

    Add files via upload

---
+++ DEEKA/php/child.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+		.a1{
+			float: left;
+		}
+	</style>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	
+	<img class="a1" src="images2/child.jpg">
+	<p><font>Many non-profit organisations are working on issues pertaining to children. But by carving a niche for itself, Save the Children has by far emerged as one of the most committed and leading independent child rights non-profit organisation. As of December 2017, they were working in 19 states of India. It started in 2008 in India, and is registered as ‘Bal Raksha Bharat’. Till date, they have changed the lives of more than 10 million (1 crore) children. Last year, they reached 22.7 lakh children in need.
+
+Save the Children believes that every child deserves a bright future. They are committed towards ensuring that children all across India should not only survive, but thrive. Save the Children is implementing programmes in the most remote corners and urban areas of the country. Their main aim is to provide quality education, healthcare, protection from harm and abuse, and life-saving aid during emergencies to children in need regardless of their caste, class, colour, race, religion, and ethnicity.
+
+British Social Reformer Eglantyne Jebb started a movement for the betterment of children in need almost a century ago. This can be dated back to the post- First World War era.  The main aim of Save the Children Fund then was to provide the much-needed relief to children who had been impacted by the war. She drafted Declaration of Rights of the Child three years later and this formed the basis of the United Nations Convention on the Rights of Child (UNCRC)- the central premise of all child rights activities around the world.
+
+Jebb believed that all children- whoever they are, wherever they are, have the right to a healthy, happy, and fulfilling life. Save the Children, India is working as an independent entity. They work for and with children in need. They are working effortlessly to provide every child with a happy childhood.
+
+</font></p>
+<img src="images2/child 2.jpg" width="100%">
+
+</body>
+</html>
---
+++ DEEKA/php/hunger.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head><style type="text/css">
+	img{
+		float: left;
+		height: 300px;
+		width: 400px;
+
+	}
+	body{background: url('images2/h.png')no-repeat 0px 0px;
+			background-size:cover  ;
+			color: white;
+
+	}
+</style>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<img src="images2/hunger.webp">
+	<h2>The Hunger Project is an international non-profit organization with a vision of “a world where every woman, man and child lead a healthy, fulfilling life of self-reliance and dignity”. The Hunger Projects implements programs in Asia, Africa and South America, that aim to mobilize rural communities to achieve sustainable progress in nutrition, family hunger, health and education.
+
+The mission of the organization is to end hunger and poverty. It currently works in 16.075 communities and empowers women and men living in rural areas to overcome hunger and poverty by themselves. The three essential elements of this organization’s work are: empowering women as key change agents, mobilizing communities for self-reliant action and fostering effective partnerships with local governments.</h2>
+<h3>Action Against Hunger is a global organization that works to end hunger around the world. The primary goal of the organization’s primary goal is to create a better way to deal with hunger and malnutrition in nearly 50 countries. The organization employs around 7.500 field staff who assist more than 21 million people in emergencies caused by food crises, natural disasters and conflicts.
+
+Action Against Hunger focuses on families with young children. Its goal is double the number of children who receive urgent hunger care, from three million to six million by 2020. The organization also deals with other issues evolving from hunger, such as nutrition and health, water and sanitation, emergency response, food security and livelihoods. Action Against Hunger saves lives of children and their families and enables them to provide for themselves. The organization constantly searches for innovative and effective solutions to push for long-term change and free world from hunger.
+</body>
+</html>
